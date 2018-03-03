1aa105372cfcc77e82c0609c541957b31c4d040a
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-03-03 22:33:24 +0100

    Creating windows and main method

---
+++ src/fr/message/Messagerie.java
@@ -0,0 +1,11 @@
+package fr.message;
+
+import fr.message.windows.ChatWindows;
+
+public class Messagerie {
+
+	public static void main(String[] args) {
+		ChatWindows userWindow = new ChatWindows();
+	}
+
+}
