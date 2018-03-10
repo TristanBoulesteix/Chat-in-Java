ed3e6f655dec9a3f30d427aeefdef5a4bb8297bc
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-03-10 15:48:52 +0100

    Creating new chat window

---
+++ src/fr/chat/windows/ChatWindows.java
@@ -0,0 +1,51 @@
+package fr.chat.windows;
+
+import java.awt.EventQueue;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+
+public class ChatWindows {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					ChatWindows window = new ChatWindows();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ChatWindows() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0 };
+		gridBagLayout.rowHeights = new int[] { 0 };
+		gridBagLayout.columnWeights = new double[] { Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { Double.MIN_VALUE };
+		frame.getContentPane().setLayout(gridBagLayout);
+	}
+
+}
